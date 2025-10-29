8b597391a28e2667d0c5965fcdfd5f64d480e896
jimboquijano <jimzqui@gmail.com>
2025-10-29 20:00:07 +0800

    GH Page Update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/orchard-test/',
   build: {
-    lib: {
-      entry: resolve(__dirname, './src/main.js'),
-      name: 'main',
-      fileName: (format) => `main.${format}.js`
-    }
+    outDir: 'dist' // must match workflow
   }
 })
