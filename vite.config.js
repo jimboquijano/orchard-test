701fd6e068e93fcc0fe01e4ac12b2ce51478a55d
jimboquijano <jimzqui@gmail.com>
2025-10-29 19:45:53 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/orchard-test/',
   build: {
     lib: {
       entry: resolve(__dirname, './src/main.js'),
